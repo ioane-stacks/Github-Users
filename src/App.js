f5ae124605211636d25a764a4934bee2376afe13
Ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2022-01-31 17:53:14 +0400

    Added in React Router, updated in react-router-dom@6.0.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Typeahead from "./Typeahead";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Typeahead />
+    <Router>
+      <Routes>
+        <Route except path="/" element={<Typeahead />} />
+      </Routes>
+    </Router>
   );
 }
 
